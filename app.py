5f3a3fcb3a6d1748f574e983ce335005e46352de
rahulkumaran <rahuldravid313@gmail.com>
2018-10-29 14:51:04 +0530

    update with various batches in alumni_batch

--- app.py
+++ app.py
@@ -20,6 +20,24 @@ def alumni():
 def alumni_batch(batch):
 	if(batch=="2020"):
 		return render_template("alumni_batch.html")
+	elif(batch=="2021"):
+		return render_template("alumni_batch.html")
+	elif(batch=="2022"):
+		return render_template("alumni_batch.html")
+	elif(batch=="2023"):
+		return render_template("alumni_batch.html")
+	else:
+		return render_template("404.html")
+
+@app.errorhandler(404) 
+def not_found(e):
+	return render_template("404.html")
+
+
+@app.errorhandler(500)
+def application_error(e):
+	return 'Sorry, unexpected error: {}'.format(e), 500
+
 
 if(__name__ == "__main__"):
 	app.run(host="localhost", port="8080")
